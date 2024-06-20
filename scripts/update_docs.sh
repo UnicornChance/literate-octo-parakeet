987611f3aef687e96adf86a54e827ee76a1aa1fa
UnicornChance <chance@defenseunicorns.com>
2024-06-20 15:37:02 -0600

    fix: remove debug logs

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -11,20 +11,14 @@ cp -r docs/* $HUGO_CONTENT_PATH
 # Iterate over each submodule directory in the submodules path
 for SUBMODULE_PATH in $SUBMODULES_PATH/*; do
 
-  echo "Submodule path : $SUBMODULE_PATH"
-
   # Initialize and update all submodules
   git submodule update --init --recursive --depth 1 $SUBMODULE_PATH
 
   if [ -d "$SUBMODULE_PATH" ]; then
     SUBMODULE_NAME=$(basename "$SUBMODULE_PATH")
-    
-    echo "Submodule name : $SUBMODULE_NAME"
 
     # Define the destination directory
     DEST_PATH="$HUGO_CONTENT_PATH/$SUBMODULE_NAME"
-
-    echo "Destination path : $DEST_PATH"
     
     # Recreate directory structure for repo submodule docs
     mkdir -p "$DEST_PATH"
@@ -33,7 +27,5 @@ for SUBMODULE_PATH in $SUBMODULES_PATH/*; do
     if [ -d "$SUBMODULE_PATH/docs" ]; then
       cp -r "$SUBMODULE_PATH/docs/"* "$DEST_PATH"
     fi
-    
-    echo "Documentation update completed for $SUBMODULE_NAME."
   fi
 done
